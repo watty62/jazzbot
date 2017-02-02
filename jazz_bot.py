19bbe5bfdcb35b6de44883b0a290620b4d58be44
Ian Watt <ianwatt@gmail.com>
2017-02-02 20:25:33 +0000

    4 main commands working

--- jazz_bot.py
+++ jazz_bot.py
@@ -1,52 +1,100 @@
 #!/usr/bin/python
-import os, time
+import os, time, sqlite3
 from slackclient import SlackClient
 from secrets import SLACK_BOT_TOKEN, BOT_ID # <== Storing my authentication there
-response = ""
 
 # constants
 AT_BOT = "<@" + BOT_ID + ">"
 WHAT_COMMAND = "what"
 ALBUM_COMMAND = "album"
 HELP_COMMAND = "help"
-HOWMANY_COMMAND = "how"
+COUNT_COMMAND = "count"
+DIE_COMMAND = "die"
+
+# gets reset by @jazzbot die command  - to kill off bot so it can be restarted
+Alive = True
 
 # instantiate Slack & Twilio clients
 slack_client = SlackClient(SLACK_BOT_TOKEN)
 
-def handle_album(commands):
+
+def handle_album(command):
+    
     """
       Process the album command
     """
-    response = "Fake response - a place holder for real DB query response\n\n"
-    response += "I have 2 albums called '*So What*'. These are by\n"
-    response += "*Al Cohn*\n"
-    response += "*Zoot Sims*\n"
+    conn = sqlite3.connect('myjazzalbums.sqlite')
+    cur = conn.cursor()
+
+    if command [-1] == "?": 
+        album_name = command[5:-1].strip()
+    else: 
+        album_name = command[5:].strip()
+    print "Album name = " + "'" + album_name +"'"
+
+    cur.execute (" SELECT Artist.name FROM Album JOIN Artist ON Artist.id = Album.artist_id WHERE Album.title =?" , (album_name + "\n",) )
+        
+    count = 0
+    response =  'Albums called ' + album_name + '\n'
+
+    for row in cur :
+        response += "Artist: " + row[0]
+        count = count + 1
+    response = "I have "+ str (count) +" " +  response
     return response
+    cur.close()
 
-def handle_howmany(commands):
-    """
-      Process the how_many command
-    """
-    # Place holder for real DB query response
-    response = "Fake response - a place holder for real DB query response\n\n"
-    response += "I have 5 albums by Charlie Parker. These are \n"
-    response += "to list these ask 'What Charlie Parker?'"
+        
 
+def handle_count(command):
+
+    """
+      Process the handle command
+    """
+
+    conn = sqlite3.connect('myjazzalbums.sqlite')
+    cur = conn.cursor()
+
+    if command [-1] == "?": 
+        artist_name = command[5:-1].strip().title()
+    else: 
+        artist_name = command[5:].strip().title()
+
+    cur.execute (" SELECT Album.title FROM Album JOIN Artist ON Artist.id = Album.artist_id WHERE Artist.name =?" , (artist_name,) )
+    #c.execute('SELECT * FROM stocks WHERE symbol=?', t)
+    count = 0
+
+    for row in cur :
+        count = count + 1
+    response = 'I have '+ str (count) + ' albums for ' + artist_name + ':\nIf you want to know the titles use the command *what* ' + artist_name
     return response
+    cur.close()
 
-def handle_what(commands):
+
+def handle_what(command):
     """
       Process the what command
     """
-    # Place holder for real DB query response
-    response = "Fake response - a place holder for real DB query response\n\n"
-    response += "I have 3 albums by Miles Davis. These are \n"
-    response += "*Bitches Brew*\n"
-    response += "*Miles Ahead*\n"
-    response += "*Filles De Kilamanjairo*"
+    
+    conn = sqlite3.connect('myjazzalbums.sqlite')
+    cur = conn.cursor()
 
+    if command [-1] == "?": 
+        artist_name = command[5:-1].strip().title()
+    else: 
+        artist_name = command[5:].strip().title()
+
+    cur.execute (" SELECT Album.title FROM Album JOIN Artist ON Artist.id = Album.artist_id WHERE Artist.name =?" , (artist_name,) )
+    #c.execute('SELECT * FROM stocks WHERE symbol=?', t)
+    count = 0
+    response =  'Albums for ' + artist_name + ':\n'
+
+    for row in cur :
+        response += row[0]
+        count = count + 1
+    response = "I have "+ str (count) +" " +  response
     return response
+    cur.close()
 
 def handle_help(commands):
     """
@@ -54,12 +102,12 @@ def handle_help(commands):
     """
     # Place holder for any attachments we want to send
     
-    response = "I'm just a collection of 1s and 0s so the famous phrase 'garbage in, garbage out' applies to me.\n So far I've evolved to understand\n"
+    response = "My capabilities are few, but generally well-executed. \nI find that when mistakes are made it is not at my end. So, to keep you right, here are some pointers:\n"
 
     # Add help message for each command
     response += "*" + HELP_COMMAND + "* - shows this message\n"
-    response += "*" + WHAT_COMMAND + "* - returns which ablbums I have by an artist\n \t e.g.: 'What Miles Davis?' will show what Miles Davis Albums I have \n"
-    response += "*" + HOWMANY_COMMAND + "* - shows you how many albums I have by an artist \n \t e.g. 'How many Charlie Parker?' will count how many Charlie Parker albums I have. \n"
+    response += "*" + WHAT_COMMAND + "* - returns which albums I have by an artist\n \t e.g.: 'What Miles Davis?' will show what Miles Davis Albums I have \n"
+    response += "*" + COUNT_COMMAND + "* - shows you how many albums I have by an artist \n \t e.g. 'Count Count Basie?' will count how many Count Basie albums I have. \n"
     response += "*" + ALBUM_COMMAND + "* - shows which albums I have of that name\n \t e.g. 'Album Stardust?'' will show which albums I have named Stardust \n"
 
     return response
@@ -70,17 +118,17 @@ def handle_command(command, channel):
         are valid commands. If so, then acts on the commands. If not,
         returns back what it needs for clarification.
     """
-    commands = command.split()
-
-
-    if commands[0].lower() == WHAT_COMMAND:
-        response = handle_what(commands)
-    elif commands[0].lower() == ALBUM_COMMAND:
-        response = handle_album(commands)
-    elif commands[0].lower() == HOWMANY_COMMAND and commands[1].lower() == 'many':
-        response = handle_howmany (commands)
-    elif commands[0].lower() == HELP_COMMAND:
-        response = handle_help (commands)
+    
+    if command.lower().startswith(WHAT_COMMAND):
+        response = handle_what(command)
+    elif command.lower().startswith(ALBUM_COMMAND):
+        response = handle_album(command)
+    elif command.lower().startswith(COUNT_COMMAND):
+        response = handle_count (command)
+    elif command.lower().startswith(HELP_COMMAND):
+        response = handle_help (command)
+    elif command.lower().startswith(DIE_COMMAND):
+        Alive = False
     else:
         response = "I'm not sure what you mean. Try *'@jazzbot help'* for more options."
 
@@ -99,7 +147,7 @@ def parse_slack_output(slack_rtm_output):
         for output in output_list:
             if output and 'text' in output and AT_BOT in output['text']:
                 # return text after the @ mention, whitespace removed
-                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                return output['text'].split(AT_BOT)[1].strip(), \
                        output['channel']
     return None, None
 
@@ -108,10 +156,11 @@ if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
     if slack_client.rtm_connect():
         print("StarterBot connected and running!")
-        while True:
+        while True and Alive :
             command, channel = parse_slack_output(slack_client.rtm_read())
             if command and channel:
                 handle_command(command, channel)
             time.sleep(READ_WEBSOCKET_DELAY)
     else:
         print("Connection failed. Invalid Slack token or bot ID?")
+
